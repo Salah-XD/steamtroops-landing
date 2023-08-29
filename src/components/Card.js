f408adca1747fcd634621061c9c7a8ee87a99cd3
Mohd. Salahudeen <hudeensala948@gmail.com>
2023-08-29 16:12:04 +0530

    c1

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,7 +1,36 @@
 import React from "react";
 
-function Card() {
-  return <div className="card "></div>;
+function Card(img, title) {
+  return (
+    <div className="card w-[210px] h-[340px] flex-col flex gap-2 bg-[#fff] shadow-lg  p-[12px] rounded-[12px]">
+      <div className="img rounded-[12px] bg-[#f2f2f2] ">
+        {/*not working */}
+        <div>
+          <img className="w-[263px] p-[24px]" src={img} alt="img" />
+        </div>
+        <div className="text text-[#fff] bg-red-600 px-5 text-[12px] py-4 text-center rounded-b-[12px]">
+          Launch offer 50 %
+        </div>
+      </div>
+      <h3
+        style={{ fontFamily: "Nunito,sans-serif", fontWeight: 900 }}
+        className="text-[14px]"
+      >
+        {title}
+      </h3>
+      <h4 className="text-[17px] font-extralight">
+        Rs. 1000s{" "}
+        <span className=" ">
+          <small>
+            <strike>Rs. 2000</strike>
+          </small>
+        </span>
+      </h4>
+      <small className="text-[9px] text-[#777777]">
+        You save Rs.1000 on launch offer
+      </small>
+    </div>
+  );
 }
 
 export default Card;
--- src/components/Shop.js
+++ src/components/Shop.js
@@ -1,7 +1,21 @@
 import React from "react";
+import Card from "./Card";
 
 function Shop() {
-  return <div className="text-[#fff]">Shop</div>;
+  return (
+    <div className="bg-[#F8F5EC] py-[40px]">
+      <h1 className="text-center font-extrabold text-[24px] mb-4">
+        Your Gateway To Knowledge And Innovation
+      </h1>
+      <p className="text-center font-light text-sm text-[#777] mb-7">
+        Discover! Shop! Elevate Your Learning With One Time shop!
+      </p>
+      <div className="card flex justify-center gap-8">
+        <Card img="./images/card1.png" title="akl;djowp" />
+      </div>
+      <a href="#">Start Shopping</a>
+    </div>
+  );
 }
 
 export default Shop;
