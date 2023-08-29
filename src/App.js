308526055f568ade21b22e960d77050b5a5cb04c
Mohd. Salahudeen <hudeensala948@gmail.com>
2023-08-29 18:24:06 +0530

    aff complete

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from "./logo.svg";
 import "./App.css";
 import Shop from "./components/Shop";
 import Present from "./components/Present";
+import Affliation from "./components/Affliation";
 
 function App() {
   return (
     <div className="App">
       <Shop />
       <Present />
+      <Affliation />
     </div>
   );
 }
--- src/components/Affliation.js
+++ src/components/Affliation.js
@@ -1,7 +1,40 @@
 import React from "react";
 
 function Affliation() {
-  return <div>Affliation</div>;
+  return (
+    <div className="aff bg-[#EEEEED] p-[70px]">
+      <h2 className="text-center font-extrabold text-[24px] mb-10">
+        Our Affliation
+      </h2>
+      <div className="img flex justify-center gap-5 ">
+        <img
+          src="./images/image 4.png"
+          alt=""
+          className="w-[220px] object-contain  	bg-[#fff] p-5 shadow-xl "
+        />
+        <img
+          src="./images/image 5.png"
+          alt=""
+          className="w-[220px] object-contain 	bg-[#fff] p-5 shadow-xl"
+        />
+        <img
+          src="./images/image 6.png"
+          alt=""
+          className="w-[220px] object-contain	bg-[#fff] p-5 shadow-xl"
+        />
+        <img
+          src="./images/image 7.png"
+          alt=""
+          className="w-[220px] object-contain	bg-[#fff] p-5 shadow-xl"
+        />
+        <img
+          src="./images/image 8.png"
+          alt=""
+          className="w-[220px] object-contain	bg-[#fff] p-5 shadow-xl"
+        />
+      </div>
+    </div>
+  );
 }
 
 export default Affliation;
