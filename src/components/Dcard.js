7086db8eacb1e3acbaf30c3826473e645493a6c7
Mohd. Salahudeen <hudeensala948@gmail.com>
2023-08-30 14:44:59 +0530

    detail complete

---
+++ src/components/Dcard.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+function Dcard() {
+  return (
+    <div>
+      <div className="card1 items-center flex flex-col justify-center">
+        <img
+          src="./images/cut.png"
+          alt=""
+          className="w-[250px] flex justify-center mb-3"
+        />
+        <h1 className="mb-3 font-bold">Learn</h1>
+        <p>
+          Bite-sized learning for targeted knowledge and skills. Learn
+          efficiently, at your own pace.
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default Dcard;
--- src/components/Detail.js
+++ src/components/Detail.js
@@ -1,40 +1,16 @@
 import React from "react";
+import Dcard from "./Dcard";
 
 function Detail() {
   return (
-    <div className="detail py-[60px]">
+    <div className="detail py-[60px] px-[60px]">
       <h1 className="text-center font-extrabold text-[24px] mb-[70px]">
         Adventure Awaits Explore, Learn, Play!
       </h1>
-      <div className="detail-content flex gap-3 justify-between">
-        <div className="card1 text-center flex flex-col justify-center">
-          <img
-            src="./images/cut.png"
-            alt=""
-            className="w-[300px] justify-center"
-          />
-          <h1>Learn</h1>
-          <p>
-            Bite-sized learning for targeted knowledge and skills. Learn
-            efficiently, at your own pace.
-          </p>
-        </div>
-        <div className="card1 text-center">
-          <img src="./images/cut.png" alt="" />
-          <h1>Exciting Experiments</h1>
-          <p>
-            Engage with the world of science, technology, and explore
-            experiments that bring theories to life.
-          </p>
-        </div>
-        <div className="card1 text-center">
-          <img src="./images/cut.png" alt="" />
-          <h1>Reusable lab</h1>
-          <p>
-            Repeat experiments, and learn from their mistakes, promoting a
-            deeper understanding of concepts.
-          </p>
-        </div>
+      <div className="detail-content flex gap-20 justify-between">
+        <Dcard />
+        <Dcard />
+        <Dcard />
       </div>
     </div>
   );
