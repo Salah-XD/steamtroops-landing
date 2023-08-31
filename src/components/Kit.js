5a7fadf6f1bc1d1fa16a7857776384a6b9873f0e
Mohd. Salahudeen <hudeensala948@gmail.com>
2023-08-31 18:58:38 +0530

    kit

--- src/components/Kit.js
+++ src/components/Kit.js
@@ -31,17 +31,17 @@ function Kit() {
           gradient={"linear-gradient(#6543EC, #4643EC, #D1008A)"}
         />
       </div>
-      <h2 className="text-center font-medium  text-[24px] mb-[40px] pt-5 text-[#EFBA00]">
+      <h2 className="text-center font-medium  text-[20px] mb-[40px] pt-5 text-[#EFBA00]">
         Discover the 'Fantastic, Remarkable' Wonders - Monthly Delivery Awaits!
       </h2>
-      <div className="steps__container flex gap-5 justify-around text-[#fff]">
+      <div className="steps__container flex justify-between text-[#fff] flex-nowrap pb-[60px]">
         <div className="steps">
           <h1>1</h1>
           <div className="sub1">
-            <div className="sub__title">Choose Your Plan</div>
+            <div className="sub__title">Choose Your Subscription</div>
             <div className="desc text-s">
-              <span>select the Subscription</span> plan that suits your Learning
-              goals and interest
+              <span>select the Subscription</span> plan that <br /> suits your
+              Learning goals and interest
             </div>
           </div>
         </div>
@@ -49,19 +49,23 @@ function Kit() {
           <h1>2</h1>
           <div className="sub2">
             <div className="sub__title">Choose Your Plan</div>
-            <div className=" desc">
-              <span>select the Subscription </span>
-              plan that suits your Learning goals and interest
+            <div className="desc">
+              Kits will be delivered to the user's <br /> address based on{" "}
+              <span>
+                monthly, <br />3 months,6 months
+              </span>{" "}
+              plan.{" "}
             </div>
           </div>
         </div>
         <div className="steps">
           <h1>3</h1>
           <div className="sub3">
-            <div className="sub__title">Choose Your Plan</div>
+            <div className="sub__title">Recieve Funfilled Kit every Month</div>
             <div className="desc">
-              <span>select the Subscription </span>plan that suits your Learning
-              goals and interest
+              <span>Access to Exclusive live classes</span> and <br />
+              <span>Pre-recorded Micro courses</span> Conducted <br />
+              Industry Professionals in Realtime
             </div>
           </div>
         </div>
